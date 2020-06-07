6ec7c0c4fc061c58d53105010bfcaee487d1dc23
Moisés Nelson Flores <nelsonflores17@gmail.com>
2020-06-07 03:13:31 -0600

    Se agregó vista de Login

--- control_impactos/views.py
+++ control_impactos/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def login(request):
+	return render(request, 'login.html')
+
 def inicio(request):
 	return render(request, 'control_impactos/index.html')
