40eb8678fbc6cd1e226be7c9c7012a7775ea8075
Moisés Nelson Flores <nelsonflores17@gmail.com>
2020-06-20 20:08:34 -0600

    Se agregó los modelos. Se habilitaron en admin

--- control_impactos/admin.py
+++ control_impactos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Supervisor, TeamManager, Empleado, Ausencia, Survey
 
 # Register your models here.
+
+admin.site.register(Supervisor)
+admin.site.register(TeamManager)
+admin.site.register(Empleado)
+admin.site.register(Ausencia)
+admin.register(Survey)
--- control_impactos/models.py
+++ control_impactos/models.py
@@ -2,41 +2,38 @@ from django.db import models
 
 # Create your models here.
 
-'''
 class Supervisor(models.Model):
 	SALID = models.CharField(max_length=10)
-	Tea_SALID = 
 	Nombre = models.CharField(max_length=150)
 	Team_Name = models.CharField(max_length=150)
-	Sup_Id = IntegerField()
 
 class TeamManager(models.Model):
 	SALID = models.CharField(max_length=10)
 	Emp_SALID = models.CharField(max_length=10)
 	Nombre = models.CharField(max_length=150)
 	Team_Name = models.CharField(max_length=150)
+	Sup_Id = models.ForeignKey(Supervisor, null= True, on_delete=models.SET_NULL)
 
 class Empleado(models.Model):
 	SALID = models.CharField(max_length=10)
 	Nombre = models.CharField(max_length=150)
-	TM_ID = 
-	Sup_ID = 
+	TM_ID = models.ForeignKey(TeamManager, null= True, on_delete=models.SET_NULL)
+	Sup_ID = models.ForeignKey(Supervisor, null= True, on_delete=models.SET_NULL)
 	Windows_User = models.CharField(max_length=150)
-	People_ID = 
+	People_ID = models.CharField(max_length=10)
 
-class Ausencias(models.Model):
+class Ausencia(models.Model):
 	ID_Ausencia = models.IntegerField()
-	SALID = models.ForeignKey(Empleado, on_delete=models.SET_NULL)
+	SALID = models.ForeignKey(Empleado, null=True, on_delete=models.SET_NULL)
 	Dia = models.DateField()
-	Compliance = 
+	Compliance = models.FloatField()
 
 class Survey(models.Model):
 	Case_id = models.CharField(max_length=25)
 	Case_Recieved = models.DateField()
 	Case_Created = models.DateField()
-	SALID = models.CharField(max_length=10)
-	Survey_Type = models.CharField(max_length=15)
+	SALID = models.ForeignKey(Empleado, null= True, on_delete=models.SET_NULL)
+	Survey_Type = models.CharField(max_length=5)# DSAT, CSAT
 	Survey_Comments = models.CharField(max_length=1024)
-	Call_ID = 
-	Itinerario = 
-'''
+	Call_ID = models.CharField(max_length=150)
+	Itinerario = models.CharField(max_length=150)
