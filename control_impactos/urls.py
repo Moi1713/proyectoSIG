f4ef102d32844b534906822d1e50c9fbfa8071ce
Moisés Nelson Flores <nelsonflores17@gmail.com>
2020-07-05 20:09:44 -0600

    Se agregó Reporte Táctico

--- control_impactos/urls.py
+++ control_impactos/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('logout/', views.logoutUser, name='logout'),
 	#Ejemplo lista
 	path('supervisors/', views.supervisors, name='supervisors'),
+	path('rendimiento/', views.rendimiento, name='rendimiento'),
 	#PDF
 	path('pdf_view/<str:type>/', views.ViewPDF, name="pdf_view"),
 	path('pdf_download/<str:type>/', views.DownloadPDF, name="pdf_download"),
--- control_impactos/views.py
+++ control_impactos/views.py
@@ -16,6 +16,9 @@ from io import BytesIO
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 
+#import para raw queries
+from django.db import connection
+
 # Create your views here.
 
 def loginPage(request):
@@ -37,6 +40,7 @@ def logoutUser(request):
 	messages.info(request, 'Gracias. Lo esperamos nuevamente.')
 	return redirect('login')
 
+#Pantallas del sistema
 @login_required(login_url='login')
 def inicio(request):
 	return render(request, 'control_impactos/index.html')
@@ -44,6 +48,19 @@ def inicio(request):
 def supervisors(request):
 	supervisors = Supervisor.objects.all()
 	return render(request, 'control_impactos/supervisors.html', { "supervisors":supervisors })
+
+def rendimiento(request):
+	#consulta
+	return render(request, 'control_impactos/tact_rend_prod.html', { "supervisors":supervisors })
+
+#convierte los resultados a Diccionario
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
 
 #Generacion de Reportes
 def render_to_pdf(template_src, context_dict={}):
@@ -58,7 +75,7 @@ def render_to_pdf(template_src, context_dict={}):
 #Descarga de Reportes
 def DownloadPDF(request, type):
 	response = ViewPDF(request, type)
-	filename = "%s_List.pdf" %(type)
+	filename = "reporte_%s.pdf" %(type)
 	content = "attachment; filename='%s'" %(filename)
 	response['Content-Disposition'] = content
 	return response
@@ -68,4 +85,7 @@ def ViewPDF(request, type):
 	if(type == 'supervisor'):
 		supervisors = Supervisor.objects.all()
 		pdf = render_to_pdf('reportes/supervisor_rep.html', {"supervisors":supervisors})
+		return HttpResponse(pdf, content_type='application/pdf')
+	if(type == 'rendimiento'):
+		pdf = render_to_pdf('reportes/tactico_rend_prod.html')
 		return HttpResponse(pdf, content_type='application/pdf')
